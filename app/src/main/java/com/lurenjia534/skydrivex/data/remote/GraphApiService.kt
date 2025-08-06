8836a38144f662a0c67bace3be1885b6a452fb17
luren <lurenjia534@outlook.com>
2025-08-07 04:29:12 +0800

    Add new API endpoints to `GraphApiService` for fetching drive root children and item-specific children

--- app/src/main/java/com/lurenjia534/skydrivex/data/remote/GraphApiService.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/data/remote/GraphApiService.kt
@@ -1,9 +1,12 @@
 package com.lurenjia534.skydrivex.data.remote
 
 import com.lurenjia534.skydrivex.data.model.DriveDto
+import com.lurenjia534.skydrivex.data.model.DriveItemsResponse
 import com.lurenjia534.skydrivex.data.model.UserDto
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GraphApiService {
     @GET("me")
@@ -11,4 +14,19 @@ interface GraphApiService {
 
     @GET("me/drive?\$select=quota")
     suspend fun getDrive(@Header("Authorization") token: String): DriveDto
+
+    @GET("me/drive/root/children")
+    suspend fun getRootChildren(
+        @Header("Authorization") token: String,
+        @Query("\$top") top: Int? = null,
+        @Query("\$orderby") orderBy: String? = null
+    ): DriveItemsResponse
+
+    @GET("me/drive/items/{itemId}/children")
+    suspend fun getChildren(
+        @Path("itemId") id: String,
+        @Header("Authorization") token: String,
+        @Query("\$top") top: Int? = null,
+        @Query("\$orderby") orderBy: String? = null
+    ): DriveItemsResponse
 }
