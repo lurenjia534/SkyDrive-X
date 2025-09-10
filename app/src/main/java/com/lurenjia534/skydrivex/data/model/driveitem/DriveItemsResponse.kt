31c92dc6edcaf89bdc0b7a8dfc9f284d98c2c336
luren <lurenjia534@outlook.com>
2025-09-10 23:49:00 +0800

    Enhance DriveItemsResponse by adding nextLink property for pagination support

--- app/src/main/java/com/lurenjia534/skydrivex/data/model/driveitem/DriveItemsResponse.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/data/model/driveitem/DriveItemsResponse.kt
@@ -1,10 +1,11 @@
 package com.lurenjia534.skydrivex.data.model.driveitem
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class DriveItemsResponse(
     val value: List<DriveItemDto>,
-    @param:com.squareup.moshi.Json(name = "@odata.nextLink")
+    @param:Json(name = "@odata.nextLink")   // 显式作用于构造参数
     val nextLink: String? = null
 )
