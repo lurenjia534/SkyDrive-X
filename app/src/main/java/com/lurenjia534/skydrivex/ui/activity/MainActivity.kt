b72eaaefcaff63505467f642f10579517d9ef5f8
luren <lurenjia534@outlook.com>
2025-09-14 00:10:56 +0800

    Update import statements to use hilt.lifecycle.viewmodel.compose for view models

--- app/src/main/java/com/lurenjia534/skydrivex/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/ui/activity/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.lurenjia534.skydrivex.ui.components.BottomNavBar
--- app/src/main/java/com/lurenjia534/skydrivex/ui/components/CopyItemSheet.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/ui/components/CopyItemSheet.kt
@@ -32,7 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.lurenjia534.skydrivex.ui.state.Breadcrumb
 import com.lurenjia534.skydrivex.ui.viewmodel.FolderPickerViewModel
 
