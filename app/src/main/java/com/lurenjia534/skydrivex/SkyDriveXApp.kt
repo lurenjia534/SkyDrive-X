f8433e1627687dff7d06586ccd0e22d7329a3145
luren <lurenjia534@outlook.com>
2025-10-14 21:33:26 +0800

    Inject `TransferTracker` into `SkyDriveXApp` for initialization during app startup.

--- app/src/main/java/com/lurenjia534/skydrivex/SkyDriveXApp.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/SkyDriveXApp.kt
@@ -1,7 +1,18 @@
 package com.lurenjia534.skydrivex
 
 import android.app.Application
+import com.lurenjia534.skydrivex.ui.notification.TransferTracker
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SkyDriveXApp : Application()
+class SkyDriveXApp : Application() {
+
+    @Inject lateinit var transferTracker: TransferTracker
+
+    override fun onCreate() {
+        super.onCreate()
+        // 强制注入以触发 TransferTracker 初始化
+        transferTracker
+    }
+}
