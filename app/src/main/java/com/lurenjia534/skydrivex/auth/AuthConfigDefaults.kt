03d094972b60ca411771185da63af7b5fa28d173
luren <lurenjia534@outlook.com>
2025-10-29 07:57:16 +0800

    Add `AuthConfigDefaults` with default authentication configuration values

---
+++ app/src/main/java/com/lurenjia534/skydrivex/auth/AuthConfigDefaults.kt
@@ -0,0 +1,9 @@
+package com.lurenjia534.skydrivex.auth
+
+object AuthConfigDefaults {
+    const val REDIRECT_URI = "msauth://com.lurenjia534.skydrivex/x5p1okZQU3tQCjGqW3O3F3psQTE%3D"
+    const val ACCOUNT_MODE = "SINGLE"
+    const val AUTHORITY_TYPE = "AAD"
+    const val AUDIENCE_TYPE = "AzureADandPersonalMicrosoftAccount"
+    const val DEFAULT_TENANT_ID = "common"
+}
