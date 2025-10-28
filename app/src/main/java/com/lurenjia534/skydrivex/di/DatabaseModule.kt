7989490edbf1bc6cc83e4b7abcd08078ca475fe7
luren <lurenjia534@outlook.com>
2025-10-29 07:57:40 +0800

    Add `AuthConfigDatabase` and `AuthConfigDao` to `DatabaseModule` for dependency injection

--- app/src/main/java/com/lurenjia534/skydrivex/di/DatabaseModule.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.lurenjia534.skydrivex.di
 
 import android.content.Context
 import androidx.room.Room
+import com.lurenjia534.skydrivex.data.local.auth.AuthConfigDao
+import com.lurenjia534.skydrivex.data.local.auth.AuthConfigDatabase
 import com.lurenjia534.skydrivex.data.local.db.TransferDatabase
 import com.lurenjia534.skydrivex.data.local.db.dao.TransferDao
 import com.lurenjia534.skydrivex.data.local.db.TransferRepository
@@ -31,6 +33,19 @@ object DatabaseModule {
 
     @Provides
     fun provideTransferDao(database: TransferDatabase): TransferDao = database.transferDao()
+
+    @Provides
+    @Singleton
+    fun provideAuthConfigDatabase(
+        @ApplicationContext context: Context
+    ): AuthConfigDatabase = Room.databaseBuilder(
+        context,
+        AuthConfigDatabase::class.java,
+        "auth_config_db"
+    ).build()
+
+    @Provides
+    fun provideAuthConfigDao(database: AuthConfigDatabase): AuthConfigDao = database.authConfigDao()
 
     @Provides
     fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
