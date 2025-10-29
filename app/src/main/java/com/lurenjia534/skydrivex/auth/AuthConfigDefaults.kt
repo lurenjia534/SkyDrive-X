4e0bdd76103e1cdfaddba6f6f1cc36ad427d238b
luren <lurenjia534@outlook.com>
2025-10-30 04:10:02 +0800

    Update `REDIRECT_URI` in `AuthConfigDefaults` and corresponding manifest path for MSAL integration adjustments

--- app/src/main/java/com/lurenjia534/skydrivex/auth/AuthConfigDefaults.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/auth/AuthConfigDefaults.kt
@@ -1,7 +1,7 @@
 package com.lurenjia534.skydrivex.auth
 
 object AuthConfigDefaults {
-    const val REDIRECT_URI = "msauth://com.lurenjia534.skydrivex/x5p1okZQU3tQCjGqW3O3F3psQTE%3D"
+    const val REDIRECT_URI = "msauth://com.lurenjia534.skydrivex/rZDXYaNZmghPivXu%2B4dDWNfayVo%3D"
     const val ACCOUNT_MODE = "SINGLE"
     const val AUTHORITY_TYPE = "AAD"
     const val AUDIENCE_TYPE = "AzureADandPersonalMicrosoftAccount"
