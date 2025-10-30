a89fd7e926ba8b60b44ef7ccefb01ed7ff012f45
luren <lurenjia534@outlook.com>
2025-10-30 09:41:16 +0800

    Bump OkHttp version to 5.2.1 in dependencies configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     ksp(libs.hilt.compiler)
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
-    implementation("com.squareup.okhttp3:okhttp:5.1.0")
+    implementation("com.squareup.okhttp3:okhttp:5.2.1")
     implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
