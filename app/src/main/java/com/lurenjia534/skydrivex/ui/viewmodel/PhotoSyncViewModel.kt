1e40f8659f01556f91f4ab133ba9a821b452a0f8
luren <lurenjia534@outlook.com>
2025-11-01 12:20:25 +0800

    Add PhotoSyncViewModel with Hilt dependency injection setup

---
+++ app/src/main/java/com/lurenjia534/skydrivex/ui/viewmodel/PhotoSyncViewModel.kt
@@ -0,0 +1,8 @@
+package com.lurenjia534.skydrivex.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PhotoSyncViewModel @Inject constructor() : ViewModel()
