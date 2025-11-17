ac701b302cec9be31fc7906abeab49df5720f5cb
luren <lurenjia534@outlook.com>
2025-11-17 22:14:46 +0800

    Update `AuthConfigDefaults` to use `MULTIPLE` account mode instead of `SINGLE`

--- app/src/main/java/com/lurenjia534/skydrivex/auth/AuthConfigDefaults.kt
+++ app/src/main/java/com/lurenjia534/skydrivex/auth/AuthConfigDefaults.kt
@@ -2,7 +2,7 @@ package com.lurenjia534.skydrivex.auth
 
 object AuthConfigDefaults {
     const val REDIRECT_URI = "msauth://com.lurenjia534.skydrivex/rZDXYaNZmghPivXu%2B4dDWNfayVo%3D"
-    const val ACCOUNT_MODE = "SINGLE"
+    const val ACCOUNT_MODE = "MULTIPLE"
     const val AUTHORITY_TYPE = "AAD"
     const val AUDIENCE_TYPE = "AzureADandPersonalMicrosoftAccount"
     const val DEFAULT_TENANT_ID = "common"
